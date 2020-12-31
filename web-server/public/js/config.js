1c30d826e3b7da520a9030c37138604e84c09982
theks <theks@163.com>
2020-12-31 11:34:43 +0800

    把开发环境的配置文件添加到git

---
+++ web-server/public/js/config.js
@@ -0,0 +1,9 @@
+var ACS_CONFIG = { 
+    pinusServer: {
+        connectorHost: "127.0.0.1",
+        connectorPort: 3050,
+        gateHost: "127.0.0.1",
+        gatePort: 3014,
+        websocketProtocol: "ws"
+    }
+}
